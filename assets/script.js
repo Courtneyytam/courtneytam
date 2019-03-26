537fb4d3ee21790e5d1e3ee213245754b4c08b1c
courtneyytam <courtneytam19@gmail.com>
2019-03-26 00:34:02 -0400

    new landing page layout with animation

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,45 @@ $(window).bind('scroll', function(){
         $('.menu').removeClass('scrolled');
     }
 });
+
+$(document).ready(function(){
+	// Add smooth scrolling to all links
+	$("a").on('click', function(event) {
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
+			// Store hash
+			var hash = this.hash;
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+			scrollTop: $(hash).offset().top
+			}, 800, function(){
+				window.location.hash = hash;
+		  });
+	} // End if
+	});
+});
+
+var selectedhome = $("#nav-home");
+var selectedcase = $("#nav-case");
+var selectedcontact = $("#nav-contact");
+
+selectedhome.on('click', function(event) {
+	selectedcase.removeClass("active");
+	selectedcontact.removeClass("active");
+	selectedhome.addClass("active");
+});
+
+selectedcase.on('click', function(event) {
+	selectedhome.removeClass("active");
+	selectedcontact.removeClass("active");
+	selectedcase.addClass("active");
+});
+
+selectedcontact.on('click', function(event) {
+	selectedcase.removeClass("active");
+	selectedhome.removeClass("active");
+	selectedcontact.addClass("active");
+});
