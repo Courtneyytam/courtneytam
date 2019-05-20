ced0a779d8f31d357a5a48902caf2d2dea1ea03d
courtneyytam <courtneytam19@gmail.com>
2019-05-20 16:57:05 -0400

    adding Smart Tagger and BigBlueButton Projects

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
-$("#KoiFinal").on("loadeddata", function() {
+$("#KoiStart").on("loadeddata", function() {
 	document.querySelector(".header-text").style.WebkitAnimationPlayState = "running";
+	/*$("#KoiStart").bind("ended", function() {
+		$("#KoiCycle").css("visibility","visible");
+	    $("#KoiCycle").play();
+	});*/
+
 });
+
+//$("#KoiStart").defaultPlaybackRate = 0.5;
 
 var timer = 0;
 var num = 150;
@@ -91,5 +98,101 @@ $(document).ready(function(){
 	}
 
 });
+
+//PROJECT RENDERING
+
+var numProjShown = 4;
+var numProjTotal = 6;
+
+var projects = {
+  "proj0": {
+    "title": "Microsoft Shared Experiences",
+    "projName": "Smart Tagger",
+    "desc": "Designing an intelligent tagging system for Office365 to improve content based search results and tag management for enterprise files.",
+    "link": "smarttagger.html",
+    "picture": "assets/smarttagger-prev.jpg",
+  },
+  "proj1": {
+    "title": "Shopify App Feature",
+    "projName": "Hatchful Onboarding",
+    "desc": "Designing an application to simplify splitting costs with other people; incorporating new technologies such as scanning and parsing.",
+    "link": "hatchful-onboarding.html",
+    "picture": "assets/onboarding-prev.jpg",
+  },
+  "proj2": {
+    "title": "Video Call Platform",
+    "projName": "Big Blue Button",
+    "desc": "Designing the interaction model of the BigBlueButton video and presentation platform to accommodate large (50+) users sessions.",
+    "link": "bigbluebutton.html",
+    "picture": "assets/bbb-prev.jpg",
+  },
+  "proj3": {
+    "title": "Shopify Internal Tool",
+    "projName": "Hatchful CMS",
+    "desc": "Designing a scalable content management system to simplify the logo upload process and to manage features and states of logos.",
+    "link": "hatchful-cms.html",
+    "picture": "assets/cms-prev.jpg",
+  },
+  "proj4": {
+    "title": "Queen's University Event",
+    "projName": "Hackathon Design",
+    "desc": "Rebranding QHacks to create a new recognizable brand image and voice that is consistent between different event products.",
+    "link": "qhacks.html",
+    "picture": "assets/qhacks-prev.jpg",
+  },
+  "proj5": {
+    "title": "Passion Project",
+    "projName": "Divide App Design",
+    "desc": "Designing an application to simplify splitting costs with other people; incorporating new technologies such as scanning and parsing.",
+    "link": "divide.html",
+    "picture": "assets/divide-prev.jpg",
+  },
+}
+
+function createProj(proj) {
+  let title = projects[proj].title;
+  let projName = projects[proj].projName;
+  let desc = projects[proj].desc;
+  let link = projects[proj].link;
+  let picture = projects[proj].picture;
+
+  var HTMLproj = `<div class='projItem ui two column grid' id="${proj}"><div class='column studyCard'><div class='studyHeader'>${title}</div><div class='studyTitle'>${projName}</div><div class='studyDesc'>${desc}</div><a class='seeMore' href="${link}">Learn more >></a></div><div class='column image-box'><a href="${link}"><img class='prev-img' src="${picture}"/></a><a class='proj-arrow' href="${link}"><img class="navigationArrow" src="assets/arrow-right_2@3x.png"></a></div></div>`
+
+  var landingStudies = document.querySelector("#landingStudies");
+  landingStudies.innerHTML += HTMLproj;
+}
+
+function deleteProj(proj) {
+  $("#" + proj).remove();
+}
+
+function showHide() {
+  let projItems = document.querySelectorAll(".projItem");
+  let numProjItems = projItems.length;
+  if (numProjItems < numProjShown+1) {
+    for (let x = numProjShown; x < numProjTotal; x++) {
+      let proj = "proj" + x;
+      createProj(proj);
+    }
+    moreProjects.innerHTML = "show less";
+  } else {
+    for (let x = numProjShown; x < numProjTotal; x++) {
+      let proj = "proj" + x;
+      deleteProj(proj);
+    }
+    moreProjects.innerHTML = "view more";
+  }
+}
+
+for (let x = 0; x < numProjShown; x++) {
+  let proj = "proj" + x;
+  createProj(proj);
+}
+
+var moreProjects = document.querySelector("#moreProjects");
+moreProjects.addEventListener('click', showHide);
+
+
+
 
 
